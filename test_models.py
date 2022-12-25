8ee4f3206e6c540104e20258f7df74d360199a48
dany_kuznetsov <dany-kuznetsov@mail.ru>
2022-12-26 01:32:42 +0200

    add tests for hw

---
+++ test_models.py
@@ -0,0 +1,35 @@
+import models_module
+import pandas as pd
+
+
+def test_model_maker_ridge_empty_params():
+    data = pd.read_excel('data/winequality-white_train.xlsx')
+    model = models_module.MyModel(model_id=-1, model_type='Ridge')
+    model.fit(data, model_params={})
+    prediction = model.predict(data)
+    print(prediction)
+    assert len(data) == len(prediction)
+    assert min(prediction) > 0
+    assert max(prediction) < 100
+
+
+def test_model_maker_ridge_with_params():
+    data = pd.read_excel('data/winequality-white_train.xlsx')
+    model = models_module.MyModel(model_id=-1, model_type='Ridge')
+    model.fit(data, model_params={'alpha': 10})
+    prediction = model.predict(data)
+    print(prediction)
+    assert len(data) == len(prediction)
+    assert min(prediction) > 0
+    assert max(prediction) < 100
+
+
+def test_model_maker_tree_with_params():
+    data = pd.read_excel('data/winequality-white_train.xlsx')
+    model = models_module.MyModel(model_id=-1, model_type='DecisionTreeClassifier')
+    model.fit(data, model_params={'max_depth': 3})
+    prediction = model.predict(data)
+    print(prediction)
+    assert len(data) == len(prediction)
+    assert min(prediction) > 0
+    assert max(prediction) < 100
